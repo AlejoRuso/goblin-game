1defa14cea1426f83ed652bd8c1c22d73e313b0f
Alejo <istominalexey@ro.ru>
2025-09-26 19:18:56 +0300

    webpack fixed

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ export default {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/goblin-game/',
     clean: true,
   },
   module: {
